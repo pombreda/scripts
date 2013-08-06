d8ba2d5ecf30552dfadd3f596cb59155bbd5a2fd
Ashwin Purohit <purohit@gmail.com>
2013-08-05 19:43:30 -0700

    no more false alarms

--- README.md
+++ README.md
@@ -4,5 +4,45 @@ scripts
 Collection of helper scripts
 
 You need a go compiler (http://golang.org/doc/install) for this to work.
+
+You need to download three files: the index-to-SNP key from the API,
+the JSON response of the call to api.23andme.com/1/genomes/, and the
+raw 23andMe data from 23andme.com. Details in the script.
+
 usage:
     go run compare_api_raw_download.go
+
+output:
+    2013/08/05 19:41:04 ApiCall: AA RawDataCall:    Total: 26
+    SNPS: rs1006094, rs10215320, rs10231034, rs10487532, rs11535222, rs12533751,
+    rs12534123,
+    rs13223129, rs13233002, rs16622, rs17163497, rs17708955, rs17767132,
+    rs2269332, rs2665033, rs2734189, rs2855951, rs2855954, rs2855983,
+    rs361379, rs7210806, rs7501783, rs7503902, rs7787291, rs8066263,
+    rs8080666,
+
+    2013/08/05 19:41:04 ApiCall: GG RawDataCall:    Total: 20
+    SNPS: rs2855945, rs12603312, rs12667623, rs12672113, rs17163396, rs17163403,
+    rs17257,
+    rs17282, rs17286, rs2040351, rs2213198, rs2213199, rs2252241,
+    rs2734151, rs2855150, rs2855943, rs361473, rs7405659, rs8065316,
+    rs8069746,
+
+    2013/08/05 19:41:04 ApiCall: CC RawDataCall:    Total: 7
+    SNPS: rs1006093, rs2040350, rs6947359, rs6968260, rs6979080, rs7789642,
+    rs8071835,
+
+
+    2013/08/05 19:41:04 ApiCall: AG RawDataCall:    Total: 4
+    SNPS: rs11079538, rs2070782, rs4968624, rs4968723,
+
+    2013/08/05 19:41:04 ApiCall: CT RawDataCall: -- Total: 1
+    SNPS: rs5929791,
+
+    2013/08/05 19:41:04 Same: 523921, Mismatches: 58, Same: 99.988930%
+
+Note that the vast majority of "mismatches" are false alarms, that
+come from hemizygous calls that have been smushed in the RawData,
+but not in the API, (i.e., ApiCall: BB, RawDataCall: B).
+They are not included in the output, but you may change that with the variable
+```FALSE_ALARMS``` in the script.
--- compare_api_raw_download.go
+++ compare_api_raw_download.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	//"bytes"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -22,6 +22,16 @@ const (
 	API_DATA_FILENAME = "apidata.txt"
 	// curl https://api.23andme.com/res/txt/snps.data
 	KEY_FILENAME = "snps.data"
+)
+
+var (
+	FALSE_ALARMS = map[string]bool{
+		"AA|A": true,
+		"CC|C": true,
+		"GG|G": true,
+		"TT|T": true,
+		"__|":  true,
+	}
 )
 
 type CallPair struct {
@@ -114,13 +124,14 @@ func getCallpairs(indexToSNP *map[int64]string,
 	}
 	var genomes GenomesEndpoint
 	json.Unmarshal(jsondata, &genomes)
-	for index := 0; index < len(genomes.Genome)/2-1; index += 2 {
+	for index := 0; index < len(genomes.Genome)/2; index += 2 {
 		api_call := fmt.Sprintf("%s%s", string(genomes.Genome[index]), string(genomes.Genome[index+1]))
 		snpstr, _ := (*indexToSNP)[int64(index/2)]
 		raw_data_call, _ := (*SNPtoCall)[snpstr]
 		snp := SNP(snpstr)
-		// Add mismatches
-		if api_call != raw_data_call {
+		// Add mismatches; some are not true mismatches
+		false_alarm := FALSE_ALARMS[fmt.Sprintf("%s|%s", api_call, raw_data_call)]
+		if (api_call != raw_data_call) && !false_alarm {
 			callpair := CallPair{ApiCall: api_call, RawDataCall: raw_data_call}
 			if _, found := callpairs[callpair]; !found {
 				callpairs[callpair] = []SNP{snp}
@@ -144,18 +155,16 @@ func printAndCalculateMismatches(callpairs map[CallPair][]SNP, correct, incorrec
 	sort.Sort(mismatches)
 	for _, mismatch := range mismatches {
 		log.Printf("ApiCall: %s\tRawDataCall: %s\tTotal: %d\t\n", mismatch.ApiCall, mismatch.RawDataCall, mismatch.Count)
-		//if mismatch.Count < 10 {
-		//buffer := bytes.Buffer{}
-		//buffer.WriteString("SNPS: ")
-		//for i, snp := range callpairs[mismatch.CallPair] {
-		//buffer.WriteString(fmt.Sprintf("%s, ", snp))
-		//if (i%6 == 0) && (i > 0) {
-		//buffer.WriteString("\n")
-		//}
-		//}
-		//buffer.WriteString("\n\n")
-		//fmt.Print(buffer.String())
-		//}
+        buffer := bytes.Buffer{}
+        buffer.WriteString("SNPS: ")
+        for i, snp := range callpairs[mismatch.CallPair] {
+            buffer.WriteString(fmt.Sprintf("%s, ", snp))
+            if (i%6 == 0) && (i > 0) {
+                buffer.WriteString("\n")
+            }
+        }
+        buffer.WriteString("\n\n")
+        fmt.Print(buffer.String())
 	}
 	log.Printf("Same: %d, Mismatches: %d, Same: %f%%", correct, incorrect, float32(correct)/float32(incorrect+correct)*100)
 }
